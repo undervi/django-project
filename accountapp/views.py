e8b828f832ad16546971490a753547320675bd25
yyebun <yujin@play3s.com>
2022-08-01 13:46:38 +0900

    djust Django course 8 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse('안녕!')
+    return render(request, 'base.html')
